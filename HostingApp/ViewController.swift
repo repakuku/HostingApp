0b8c40ad4b81373ca75332cf57f683be7a28a4c1
Alexey Turulin <repakuku@icloud.com>
2023-09-11 17:39:30 -0400

    Initial Commit

---
+++ HostingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HostingApp
+//
+//  Created by Alexey Turulin on 9/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
